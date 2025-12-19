33f942ef32f494d03d83d5ca1d1ea96487a9f3a9
jlbuild <juliabuildbot@gmail.com>
2025-12-19 18:06:17 +0000

    Eigen_jll build 3.4.1+0

--- src/Eigen_jll.jl
+++ src/Eigen_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Eigen")
-JLLWrappers.@generate_main_file("Eigen", UUID("bc6bbf8a-a594-5541-9c57-10b0d0312c70"))
+JLLWrappers.@generate_main_file("Eigen", Base.UUID("bc6bbf8a-a594-5541-9c57-10b0d0312c70"))
 end  # module Eigen_jll
